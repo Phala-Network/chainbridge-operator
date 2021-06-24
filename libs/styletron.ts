a442ef4b675299b7e25487c41cf7fe635281452f
amphineko <sukareki@gmail.com>
2021-06-24 20:33:42 +0800

    re(styletron): extract hydrate class name
    Signed-off-by: amphineko <sukareki@gmail.com>

--- libs/styletron.ts
+++ libs/styletron.ts
@@ -1,8 +1,9 @@
 import { Client, Server } from 'styletron-engine-atomic'
 
-const getHydrateClass = (): HTMLStyleElement[] => {
-    const elements = [...document.getElementsByClassName('_styletron_hydrate_')]
-    return elements.filter((element) => element instanceof HTMLStyleElement) as HTMLStyleElement[]
+export const HydrateClass = '_styletron_hydrate_'
+
+const getHydrateClass = (): HTMLCollectionOf<HTMLStyleElement> => {
+    return document.getElementsByClassName(HydrateClass) as HTMLCollectionOf<HTMLStyleElement>
 }
 
 export const styletron = typeof window === 'undefined' ? new Server() : new Client({ hydrate: getHydrateClass() })
