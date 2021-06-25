881c4d1d09ea5ef026e02d2346f5da5b02694bb1
amphineko <sukareki@gmail.com>
2021-06-26 00:26:47 +0800

    feat(coms/AllowanceGrant): grant to infinite
    Signed-off-by: amphineko <sukareki@gmail.com>

--- components/ethereum/AllowanceGrant.tsx
+++ components/ethereum/AllowanceGrant.tsx
@@ -7,14 +7,14 @@ import { useEthers } from '../../libs/ethereum/contexts/useEthers'
 import { useErc20Contract } from '../../libs/ethereum/erc20/useErc20Contract'
 import { useErc20AssetHandlerAllowanceQuery } from '../../libs/ethereum/queries/useErc20AllowanceQuery'
 
-export const AllowanceApprove = ({ owner, value }: { owner: string; value: BigNumber }): JSX.Element => {
+export const AllowanceApprove = ({ owner }: { owner: string; value: BigNumber }): JSX.Element => {
     const { contract } = useErc20Contract()
     const { data: allowance } = useErc20AssetHandlerAllowanceQuery(owner)
     const { provider, signer } = useEthers()
 
     const allowanceText = useMemo(() => allowance !== undefined && ethers.utils.formatUnits(allowance, 18), [allowance]) // TODO: extract this hardcoded 18
 
-    const startApprove = () => {
+    const startApprove = async () => {
         const network = networks[provider?.network.chainId as number]
 
         if (contract === undefined || network === undefined || signer === undefined) {
@@ -23,7 +23,9 @@ export const AllowanceApprove = ({ owner, value }: { owner: string; value: BigNu
 
         const contractSigned = contract.connect(signer)
 
-        contractSigned.functions['approve']?.(network.erc20handler, value)
+        // TODO: add transaction progress indication
+
+        return contractSigned.functions['approve']?.(network.erc20Handler, ethers.utils.parseUnits('11451419810', 18))
     }
 
     return (
