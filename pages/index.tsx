aaa5de3d5b24a59bd077e307f31fb21272ef6bb4
amphineko <sukareki@gmail.com>
2021-06-26 10:10:45 +0800

    feat: navigate to to-substrate on landing

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,15 @@
+import { useRouter } from 'next/dist/client/router'
+import { useEffect } from 'react'
+
 const IndexPage = (): JSX.Element => {
+    const { pathname, push } = useRouter()
+
+    useEffect(() => {
+        if (pathname === '/') {
+            push('/to-substrate').catch(() => {})
+        }
+    })
+
     return <></>
 }
 
