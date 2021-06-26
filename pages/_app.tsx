768637d0d92c7051a24507bcc813467543b9e934
amphineko <sukareki@gmail.com>
2021-06-26 10:09:17 +0800

    feat(_app): page navigation (#7)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import { BaseProvider, LightTheme } from 'baseui'
+import { AppNavBar, NavItemT, setItemActive } from 'baseui/app-nav-bar'
 import type { AppProps } from 'next/app'
+import { useRouter } from 'next/dist/client/router'
 import React, { useMemo } from 'react'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Provider as StyletronProvider } from 'styletron-react'
@@ -10,8 +12,35 @@ import { ApiPromiseProvider } from '../libs/polkadot/hooks/useApiPromise'
 import { Web3Provider as PolkadotWeb3Provider } from '../libs/polkadot/hooks/useWeb3'
 import { styletron } from '../libs/styletron'
 
+interface NavigationItem extends NavItemT {
+    label: string
+    navigate: () => void
+}
+
 const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
     const client = useMemo(() => new QueryClient(), [])
+
+    const { pathname, push } = useRouter()
+
+    const mainItems = useMemo(
+        () => [
+            {
+                active: pathname === '/to-substrate',
+                label: 'ERC-20 to Phala',
+                navigate: () => {
+                    push('/to-substrate').catch(() => {})
+                },
+            },
+            {
+                active: pathname === '/to-ethereum',
+                label: 'Phala to ERC-20',
+                navigate: () => {
+                    push('/to-ethereum').catch(() => {})
+                },
+            },
+        ],
+        [pathname, push]
+    )
 
     return (
         <QueryClientProvider client={client}>
@@ -21,6 +50,12 @@ const MyApp = ({ Component, pageProps }: AppProps): JSX.Element => {
                         <ApiPromiseProvider endpoint={polkadot.endpoint} registryTypes={polkadot.typedefs}>
                             <StyletronProvider value={styletron}>
                                 <BaseProvider theme={LightTheme}>
+                                    <AppNavBar
+                                        mainItems={mainItems}
+                                        onMainItemSelect={(item) => {
+                                            ;(item as NavigationItem).navigate()
+                                        }}
+                                    />
                                     <Component {...pageProps} />
                                 </BaseProvider>
                             </StyletronProvider>
