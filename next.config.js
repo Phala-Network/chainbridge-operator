4fd2d2be0202dc89fe7ebb034e92e851f2fc1f81
amphineko <sukareki@gmail.com>
2021-07-01 16:36:06 +0800

    Revert "[CI] chore(next.config): set target to serverless"
    This reverts commit afd3f47b9001b5f94ccf44137841b1ed9d1a635e.

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 module.exports = {
     reactStrictMode: true,
-    target: 'serverless',
     webpack: (config) => {
         config.externals = config.externals || {}
         config.externals['styletron-server'] = 'styletron-server'
